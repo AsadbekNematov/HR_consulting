a676cdc9f4b2ebee50b3c271bc3b9c51fce00355
Asadbek <asadbekn443@gmail.com>
2023-05-26 19:51:53 -0400

    navbar

--- hr_consulting/src/components/Hero.jsx
+++ hr_consulting/src/components/Hero.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import styles from '../style';
-import {disocunt, robot} from '../assets';
+import {discount9, robot} from '../assets';
 
 const Hero = () =>
 (
   <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY} `}>
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
       <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
-        <img src={disocunt} alt="discount" className='w-[32px] h-[32px]' />
+        <img src={discount} alt="discount" className='w-[32px] h-[32px]' />
       </div>
     </div>
   </section>
