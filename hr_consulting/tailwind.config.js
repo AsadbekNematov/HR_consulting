5a9fe38ce266ba2e7630a4c91524ff36b31117d3
Asadbek <asadbekn443@gmail.com>
2023-05-25 21:25:27 -0400

    first commit

---
+++ hr_consulting/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#DADADA",
+        secondary: "#08FFC8",
+        dimWhite: "#FFF7F7",
+        dimBlue: "#204969",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
