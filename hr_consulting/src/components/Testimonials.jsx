ae4ce43b7f7fa4ea98b85ce810b7c9ce6fe8a38f
Asadbek <asadbekn443@gmail.com>
2023-05-30 23:27:54 -0400

    Testimonials

--- hr_consulting/src/components/Testimonials.jsx
+++ hr_consulting/src/components/Testimonials.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { feedback } from '../constants';
+import styles from '../style';
+import FeedbackCard from "./FeedbackCard"
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <section>
+    <div>
+      
+    </div>
+  </section>
+)
 
 export default Testimonials
