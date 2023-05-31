6c045f2d6464fe9e76af8d972d0424b645c16ed7
Asadbek <asadbekn443@gmail.com>
2023-05-30 23:32:41 -0400

    Testimonials

--- hr_consulting/src/components/Testimonials.jsx
+++ hr_consulting/src/components/Testimonials.jsx
@@ -4,9 +4,15 @@ import styles from '../style';
 import FeedbackCard from "./FeedbackCard"
 
 const Testimonials = () => (
-  <section>
-    <div>
-      
+  <section className={`${styles.paddingY} ${styles.flexCenter}`} id='clients'>
+    {/*To DO*/}
+    <div />
+
+    <div className='w-full flex flex-col justify-between items-center md:flex-row sm:mb-16 mb-6 relative z-[1]'>
+      <h1>
+        What people are <br className='sm:block hidden' />
+        saying about us.
+      </h1>
     </div>
   </section>
 )
